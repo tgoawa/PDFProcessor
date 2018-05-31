06a702c84096447e77d0bce3c62340abe5b8741d
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-05-31 09:41:42 -0500

    Added missing function to fix form

--- TaureauPDFProcessor/Form1.cs
+++ TaureauPDFProcessor/Form1.cs
@@ -14,6 +14,7 @@ namespace TaureauPDFProcessor
     {
         public Form1()
         {
+            InitializeComponent();
             this.projectNameTextBox = new TextBox();
             this.passwordTextBox = new TextBox();
         }
