5b60564c18494254ce8ac51f9fdf43f33113839c
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-05-31 10:38:59 -0500

    create beginning of project model

---
+++ TaureauPDFProcessor/ProjectModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaureauPDFProcessor
+{
+    class ProjectModel
+    {
+        public int Id { get; set; }
+        public string PdfFile { get; set; }
+        public string[] CompanyNames { get; set; }
+        public string Password { get; set; }
+        private string MasterPassword = "masterpassword";
+    }
+}
