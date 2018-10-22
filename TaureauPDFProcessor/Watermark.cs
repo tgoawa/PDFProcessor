de731bc0787dc267d6917fcb8413e9c00a1ff12f
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-10-22 10:03:48 -0500

    set watermark to only display name once

--- TaureauPDFProcessor/Watermark.cs
+++ TaureauPDFProcessor/Watermark.cs
@@ -15,18 +15,29 @@ namespace TaureauPDFProcessor
         {
             foreach (PdfPageBase page in doc.Pages)
                 {
-                    PdfTilingBrush brush = new PdfTilingBrush(new SizeF(page.Canvas.ClientSize.Width / 2, page.Canvas.ClientSize.Height / 3));
-                    brush.Graphics.SetTransparency(0.3f);
-                    brush.Graphics.Save();
-                    brush.Graphics.TranslateTransform(brush.Size.Width / 2, brush.Size.Height / 2);
-                    brush.Graphics.RotateTransform(-45);
-                    brush.Graphics.DrawString(companyName,
-                        new PdfFont(PdfFontFamily.Helvetica, 24), PdfBrushes.Gray, 0, 0,
-                        new PdfStringFormat(PdfTextAlignment.Center));
-                    brush.Graphics.Restore();
-                    brush.Graphics.SetTransparency(1);
-                    page.Canvas.DrawRectangle(brush, new RectangleF(new PointF(0, 0), page.Canvas.ClientSize));
-                }
+                PdfTilingBrush brush = new PdfTilingBrush(new SizeF(page.Canvas.ClientSize.Width, page.Canvas.ClientSize.Height));
+                brush.Graphics.SetTransparency(0.3f);
+                brush.Graphics.Save();
+                brush.Graphics.TranslateTransform(100, 200);
+                brush.Graphics.RotateTransform(-45);
+                brush.Graphics.DrawString(companyName,
+                    new PdfFont(PdfFontFamily.Helvetica, 16), PdfBrushes.Purple, 0, 0,
+                    new PdfStringFormat(PdfTextAlignment.Left));
+                brush.Graphics.Restore();
+                brush.Graphics.SetTransparency(1);
+                page.Canvas.DrawRectangle(brush, new RectangleF(new PointF(0, 0), page.Canvas.ClientSize));
+                //float calculateDistance = (page.Canvas.ClientSize.Height - 80) / 3;
+                //float height = 40;
+                //for (var count = 0; count < 4; count++)
+                //{
+                //page.Canvas.DrawString(companyName,
+                //    new PdfFont(PdfFontFamily.Helvetica, 25f),
+                //    new PdfSolidBrush(Color.Green),
+                //    10, height);
+                //    height = height + calculateDistance;
+                //}
+                //page.Canvas.RotateTransform(-20);
+            }
             return Task.FromResult(doc);
 
         }
